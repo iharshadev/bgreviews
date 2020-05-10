f1f0bf0e8b13855e653033139e65533bf5ad2e85
Harsha <harshavardhanr.993@gmail.com>
2020-05-09 21:42:08 -0500

    Reading the model from pickled file for prediction

--- bgreview/views.py
+++ bgreview/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+import pickle
+
+from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def get(request):
@@ -9,8 +11,12 @@ def get(request):
 
 @api_view(['POST'])
 def post_test(request):
-    if request.method == 'POST':
-        val = request.POST.get("key")
-    else:
-        val = "Wrong request type"
-    return Response(val)
+    if request.method != 'POST':
+        return JsonResponse({"response": "Wrong request type. Please use post"})
+    comment = request.POST.get("key")
+    model = pickle.load(open("bgreview/model/model.pkl", "rb"))
+    vocabulary = pickle.load(open("bgreview/model/vocab.pkl", "rb"))
+    vectorizer = TfidfVectorizer(vocabulary=vocabulary)
+    comment = vectorizer.fit_transform([comment])
+    prediction = model.predict(comment)
+    return JsonResponse({"response": round(prediction[0], 1)})
